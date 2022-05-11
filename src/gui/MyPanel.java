d03fdbc33736e2a53904f89444ee6cdd2f2cfc2b
Eric Shen <eric.yan.shen@gmail.com>
2022-05-11 13:03:44 -0700

    Begin to interpret clicks (not drags yet) on board

--- src/gui/MyPanel.java
+++ src/gui/MyPanel.java
@@ -1,53 +1,40 @@
 package gui;
 
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
 // import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 import backend.LetterGrid;
+import backend.Selection;
 
 public class MyPanel extends JPanel {
-    // private Image image;
+
     private int gridSize;
+    private int cellSize;
     private LetterGrid grid;
+    private Selection sel;
     
     public MyPanel() {
         this(4);
     }
 
     public MyPanel(int gridSize) {
-        // image = new ImageIcon("sky.png").getImage();
         this.setPreferredSize(new Dimension(500, 500));
         this.gridSize = gridSize;
+        cellSize = 400 / gridSize;
         grid = new LetterGrid(gridSize);
+
+        implementMouseListeners();
     }
 
     public void paint(Graphics g) {
         Graphics2D g2D = (Graphics2D) g;
 
-        // g2D.drawImage(image, 0, 0, null);
-        
-        // g2D.setPaint(Color.BLUE);
-        // g2D.setStroke(new BasicStroke(5));
-        // g2D.drawLine(0, 0, 500, 500);
-        // g2D.setPaint(Color.PINK);
-        // g2D.fillRect(0, 0, 100, 200);
-        // g2D.setPaint(Color.CYAN);
-        // g2D.drawRect(0, 200, 100, 200);
-
-        // g2D.setPaint(Color.ORANGE);
-        // g2D.drawOval(0, 0, 100, 100);
-
-        // g2D.setPaint(Color.RED);
-        // g2D.fillArc(0, 0, 100, 100, 0, 180);
-        // g2D.setPaint(Color.WHITE);
-        // g2D.fillArc(0, 0, 100, 100, 180, 180);
-
-        // int[] xPoints = {150, 250, 350};
-        // int[] yPoints = {300, 150, 300};
         g2D.setPaint(Color.BLUE);
-        // g2D.fillPolygon(xPoints, yPoints, 3);
 
         // g2D.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 50));
         // g2D.drawString("Word Royale", 100, 50);
@@ -56,7 +43,6 @@ public class MyPanel extends JPanel {
     }
 
     public void drawLetterGrid(Graphics2D g2D) {
-        int cellSize = 400 / gridSize;
         g2D.setFont(new Font(Font.MONOSPACED, Font.PLAIN, cellSize / 2));
         for (int i = 0; i < gridSize; i++) {
             for (int j = 0; j < gridSize; j++) {
@@ -70,5 +56,41 @@ public class MyPanel extends JPanel {
 
     public void setGridSize(int size) {
         gridSize = size;
+    }
+
+    private void implementMouseListeners() {
+        addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent mouseEvent) {
+                sel = new Selection(grid, (mouseEvent.getX() - 50) / cellSize, (mouseEvent.getY() - 65) / cellSize);
+                System.out.println(sel.word());
+            }
+
+            @Override
+            public void mousePressed(MouseEvent mouseEvent) {
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent mouseEvent) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent mouseEvent) {}
+
+            @Override
+            public void mouseExited(MouseEvent mouseEvent) {}
+        });
+        addMouseMotionListener(new MouseMotionListener() {
+            @Override
+            public void mouseDragged(MouseEvent mouseEvent) {
+
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent mouseEvent) {
+
+            }
+        });
     }
 }
